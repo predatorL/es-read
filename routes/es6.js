d30899475594021a8d9eeda3b03c9c4e2b8e61d2
lhy90s <github_search@sina.com>
2017-09-25 04:08:41 -0700

    project init

---
+++ routes/es6.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+  res.render('es6/index');
+})
+
+router.get('/:page', function (req, res, next) {
+  const {
+    page
+  } = req.params
+  res.render(`es6/${page}`)
+})
+
+
+module.exports = router
