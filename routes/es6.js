97b86e7eb96a24bbd82a3e1f615dbcfc3c320cab
predatorL <github_search@sina.com>
2017-10-08 08:58:35 -0700

    es6 route

--- routes/es6.js
+++ routes/es6.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res, next) {
-  res.render('es6/index');
-})
 
 router.get('/:page', function (req, res, next) {
   const {
-    page
+    page = 'index'
   } = req.params
   res.render(`es6/${page}`)
 })
