0de27b860932ea518d1915ffc158f95f3d7b11d8
predatorL <github_search@sina.com>
2017-10-12 09:09:36 -0700

    add let

--- routes/es6.js
+++ routes/es6.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 
+router.get('/', function (req, res, next) {
+  res.render('es6/index')
+})
 router.get('/:page', function (req, res, next) {
   const {
     page = 'index'
